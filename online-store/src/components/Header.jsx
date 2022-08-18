9834e9cd551ba04754bfe995267fdfacea4d587d
gabrielraeder <gabrielraeder@outlook.com.br>
2022-08-17 22:39:29 -0300

    link no nome da página

--- online-store/src/components/Header.jsx
+++ online-store/src/components/Header.jsx
@@ -10,7 +10,9 @@ export default class Header extends Component {
 
     return (
       <header className="flex head">
-        <h1 className="flexColumn centered headTitle"> FrontEnd Online Store</h1>
+        <Link to="/" className="titleLink">
+          <h1 className="flexColumn centered headTitle">FrontEnd Online Store</h1>
+        </Link>
           <SearchBar
             searchProducts={ searchProducts }
             handleChange={ handleChange }
